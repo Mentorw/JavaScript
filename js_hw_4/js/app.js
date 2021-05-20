ebd778adb4ed809b4d3beb729bca1b5b0e04673d
Mentorw <mgolovcenko890@gmai.com>
2021-05-20 18:35:35 +0300

    homework 5

--- js_hw_4/js/app.js
+++ js_hw_4/js/app.js
@@ -1,42 +1,22 @@
-console.table(countries);
-//task 1
-function getInfoCounties(arr, [field1, field2, field3]) {
-    let info = arr.map(function (country) {
-        return { [field1]: country[field1], [field2]: country[field2], [field3]: country[field3] };
-    })
-    return info;
+let click = document.getElementById('btn');
+click.onclick = function () {
+    let user = {};
+    user.name = document.getElementById('name').value;
+    user.email = document.getElementById('email').value;
+    user.age = document.getElementById('age').value;
+    user.comment = document.getElementById('comment').value;
+    user.country = document.getElementById('country').value;
+    console.log('user:' + user);
+    return false;
 }
-let arr = prompt('input 3 fields:');
-arr = arr.split(',');
-console.table(getInfoCounties(countries, arr));
-
-//task 2
-function getSortData(arr, str) {
-    let sorted = arr.sort(function (countryA, countryB) {
-        if (typeof (countryA[str]) == "string") {
-            return countryA[str] - countryB[str];
-        } else if (typeof (countryA[str]) == "number") {
-            return countryB[str] - countryA[str];
-        }
-    })
-    return sorted;
-}
-let field = prompt('input field for sort');
-console.table(getSortData(countries, field));
-
-//task 3
-function getAvarage(arr, str) {
-    let count = 0;
-    let sum = arr.reduce(function (acc, el) {
-        if (typeof (el[str]) == "number" && !isNaN(el[str])) {
-            count++;
-            return acc + el[str];
-        }
-      
-    }, 0)
-    return (sum / count).toFixed(2);
-}
-
-let average = prompt('input field')
-console.log(average + ": " + getAvarage(countries, average));
-// Выбивает NaN, если в объекте есть хоть одно нечисловое значение, не смог решить даную проблему
+// Альтернативное решение
+/*document.forms.form.onsubmit = function(){
+    let user = {};
+    user.name = this.name.value;
+    user.email = this.email.value;
+    user.age = this.age.value;
+    user.comment = this.comment.value;
+    user.country = this.country.value;
+    console.log('user:' + user);
+    return false;
+}*/
