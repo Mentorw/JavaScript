528ebd7cb5b539f86ebfc8ab0088239f489cb624
Mentorw <mgolovcenko890@gmai.com>
2021-06-01 18:04:43 +0300

    Task 8(classes)

--- js_lection_6/js/app.js
+++ js_lection_6/js/app.js
@@ -1,100 +1,56 @@
-window.onload = function () {
-    window.countries = null;
-
-    const setListeners = () => {
-        $('#search').on('keyup', e => {
-            let value = e.target.value;
-            value = value.toLowerCase();
-            let filteredCountries = window.countries.filter(country => country.name.toLowerCase().indexOf(value) > -1
-                || country.capital.toLowerCase().indexOf(value) > -1
-                || country.region.toLowerCase().indexOf(value) > -1);
-
-            renderCountries(filteredCountries);
-        });
-
-        $('table thead th.bg-warning').on('click touch', e => {
-            let field = e.currentTarget.innerText;
-            field = field.toLowerCase();
-            let numerableFields = ['population', 'area'];
-            let sortedCountries = window.countries.sort((a, b) => {
-                if (numerableFields.indexOf(field) > -1) {
-                    return a[field] < b[field];
-                }
-                return a[field] > b[field];
-
-                // return numerableFields.indexOf(field) > -1 ? a[field] < b[field] : a[field] > b[field];
-            });
-            $('table thead th.bg-warning').removeClass('bg-danger text-white');
-            $(e.currentTarget).addClass('bg-danger text-white')
-            renderCountries(sortedCountries);
-        });
-
-        $('td').on('click', e => {
-            $('table tbody tr').removeClass('bg-dark text-white');
-            $(e.currentTarget).parent('tr').addClass('bg-dark text-white');
-        });
-        $("select").change(e => {
-            $('#search').val("");
-            let selectedCountry = $("select option:selected").text();
-            let findCountry = window.countries.filter(country => country.name === selectedCountry);
-            renderCountries(findCountry);
-
-        });
-        $('#search').autocompleted({
-            source: tags
-        });
-
-    }
-    const renderOption = countries => {
-        let selectStr = '<option value="">Not selected</option>';
-        for (let country of countries) {
-            selectStr += `<option value="${country.name}">${country.name}</option>`;
-        }
-        $('.countries-select').html(selectStr);
-    }
-    const renderCountries = countries => {
-        let htmlStr = '';
-        let tags = [];
-        for (let country of countries) {
-            htmlStr += `<tr>
-             <td>${country.name}</td>
-             <td>${country.capital}</td>
-             <td>${country.region}</td>
-             <td>${country.population}</td>
-             <td>${country.area}</td>
-             <td>${country.currencies}</td>
-             <td>${country.languages}</td>
-             <td><img src="${country.flag}"  height="30px"/></td>
-         </tr>`;
-            tags.push(country.name);
-
-        }
-        if (!countries.length) {
-            htmlStr = '<tr><td colspan="8" class="text-center">Не найдено</td></tr>'
-        }
-        $('table > tbody').html(htmlStr);
-
-    };
-
-    const getData = () => {
-        let countries = [];
-        fetch('https://restcountries.eu/rest/v2/all')
-            .then(res => res.json()).then(data => {
-                countries = data.map(country => {
-                    let { name, population, capital, area, currencies, languages, flag, region } = country;
-                    return {
-                        name, population, capital: capital || '---', flag, region: region || '---',
-                        area: area || 0,
-                        currencies: currencies.map(el => el.name).join(', '),
-                        languages: languages.map(el => el.name).join(', ')
-                    };
-                });
-                window.countries = countries;
-                renderCountries(countries);
-                renderOption(countries);
-                setListeners();
-            });
+class Auto {
+    constructor(brand, model, year, vinCode) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.vinCode = vinCode;
     }
 
-    getData();
+    log() {
+        console.log("Brand: " + this.brand + "\nModel: " + this.model + "\nYear: " + this.year);
+    }
+
+    checkVin() {
+        console.log(String(this.vinCode).length === 16 ? 'true' : 'false');
+        return String(this.vinCode).length === 16 ? 'true' : 'false';
+    }
 }
+
+class Auto_Fuel extends Auto {
+    constructor(brand, model, year, vinCode, engineDisplacement, fuelConsumption) {
+        super(brand, model, year, vinCode);
+        this.engineDisplacement = engineDisplacement;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    showFuelConsumption() {
+        console.log("Engine displacement: " + this.engineDisplacement + "\nFuel consumption: " + this.fuelConsumption);
+        return this.brand + " " + this.model + "\nEngine displacement: " + this.engineDisplacement + "\nFuel consumption: " + this.fuelConsumption;
+    }
+}
+
+class Auto_Electric extends Auto {
+    constructor(brand, model, year, vinCode, batteryCapacity) {
+        super(brand, model, year, vinCode);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    showBatteryConfig() {
+        console.log("Battery capacity: " + this.batteryCapacity);
+        return this.brand + " " + this.model + "\nBattery capacity: " + this.batteryCapacity;
+    }
+}
+
+let honda = new Auto("HONDA", "	CR-V 2.4 RVSi", 2007, "JHLRE48577C41549");
+honda.log();
+honda.checkVin();
+
+let bmw = new Auto_Fuel("BMW", "E-85(Z4)", 2004, "4USBT53544LT26841", "2.9L", 9.1);
+bmw.log();
+bmw.checkVin();
+bmw.showFuelConsumption();
+
+let tesla = new Auto_Electric("Tesla", "Model X", 2020, "AF12124AEGJ32413", "100 кВт/ч");
+tesla.log();
+tesla.checkVin();
+tesla.showBatteryConfig();
